82f5314dc9eca80707f8e3569f829dfb1793fd41
Muammer Akca <muammerakca10@gmail.com>
2022-08-29 15:44:23 +0300

    Refactored ViewController

--- instagramClone/Controllers/ViewController.swift
+++ instagramClone/Controllers/ViewController.swift
@@ -21,34 +21,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func loginButton(_ sender: Any) {
-        if emailTextField.text != "" && passwordTextField.text != "" {
-            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authDataResult, error) in
-                
-                if error != nil {
-                    GiveError.giveErrorMessage(title: "Error", message: error?.localizedDescription ?? "Something went wrong" , vc: self)
-                } else {
-                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
-                }
-            }
-        } else {
+//        if emailTextField.text != "" && passwordTextField.text != "" {
+//            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authDataResult, error) in
+//
+//                if error != nil {
+//                    GiveError.giveErrorMessage(title: "Error", message: error?.localizedDescription ?? "Something went wrong" , vc: self)
+//                } else {
+//                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+//                }
+//            }
+//        } else {
+//            GiveError.giveErrorMessage(title: "Error", message: "Enter a valid email and password", vc: self)
+//        }
+        if emailTextField.text == "" || passwordTextField.text == "" {
             GiveError.giveErrorMessage(title: "Error", message: "Enter a valid email and password", vc: self)
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authDataResult, error) in
+            
+            if error != nil {
+                GiveError.giveErrorMessage(title: "Error", message: error?.localizedDescription ?? "Something went wrong" , vc: self)
+                return
+            }
+            self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+            
         }
     }
     
     
     @IBAction func signupButton(_ sender: Any) {
         
-        if emailTextField.text != "" && passwordTextField.text != "" {
-            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authdataresult, error) in
-                if error != nil {
-                    GiveError.giveErrorMessage(title: "Error", message: error?.localizedDescription ?? "Something went wrong" , vc: self)
-                } else {
-                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
-                }
-            }
-                
-        } else {
+        if emailTextField.text == "" || passwordTextField.text == "" {
             GiveError.giveErrorMessage(title: "Error", message: "Enter a valid username and password", vc: self)
+            return
+        }
+            
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authdataresult, error) in
+            if error != nil {
+                GiveError.giveErrorMessage(title: "Error", message: error?.localizedDescription ?? "Something went wrong" , vc: self)
+                return
+            }
+            self.performSegue(withIdentifier: "toFeedVC", sender: nil)
         }
     }
     
--- instagramClone/Helpers/Post.swift
+++ instagramClone/Helpers/Post.swift
@@ -8,17 +8,7 @@
 import Foundation
 
 struct Post {
-    
     var email : String
     var comment : String
     var imageURL : String
-    
-    /*
-     init(email : String, comment : String, imageURL : String) {
-        self.email = email
-        self.comment = comment
-        self.imageURL = imageURL
-     
-     }
-     */
 }
