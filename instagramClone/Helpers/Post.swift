85051fa9159c70438eec2c32f4f20fd6fa50bbec
Muammer Akca <muammerakca10@gmail.com>
2022-08-29 15:33:59 +0300

    Post class converted to Post struct

--- instagramClone/Helpers/Post.swift
+++ instagramClone/Helpers/Post.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 
-class Post {
+struct Post {
     
     var email : String
     var comment : String
     var imageURL : String
     
-    init(email : String, comment : String, imageURL : String) {
+    /*
+     init(email : String, comment : String, imageURL : String) {
         self.email = email
         self.comment = comment
         self.imageURL = imageURL
-    }
+     
+     }
+     */
 }
