6ac295d76cedc471b8c6727311428a1b7345e253
Muammer Akca <muammerakca10@gmail.com>
2022-08-30 12:47:12 +0300

    Added logout symbol to navigation bar instead of logout button

--- instagramClone/Controllers/SettingsViewController.swift
+++ instagramClone/Controllers/SettingsViewController.swift
@@ -12,14 +12,28 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutBarButtonTapped))
+         
+         
     }
-
-    @IBAction func logoutButtonClicked(_ sender: Any) {
-        do {
-            try Auth.auth().signOut()
-            performSegue(withIdentifier: "toFeedVC", sender: nil)
-        } catch {
-            GiveError.giveErrorMessage(title: "Error", message: "Could not log out", vc: self)
-        }
+    
+    @objc private func logoutBarButtonTapped(){
+        let alertLogout = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: UIAlertController.Style.alert)
+        
+        alertLogout.addAction(UIAlertAction(title: "Yes", style: UIAlertAction.Style.default, handler: { (action : UIAlertAction) in
+            do {
+                try Auth.auth().signOut()
+                self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+            } catch {
+                GiveError.giveErrorMessage(title: "Error", message: "Could not log out", vc: self)
+            }
+        }))
+        
+        alertLogout.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: { (action: UIAlertAction) in
+            alertLogout.dismiss(animated: true)
+        }))
+        
+        present(alertLogout, animated: true)
     }
 }
